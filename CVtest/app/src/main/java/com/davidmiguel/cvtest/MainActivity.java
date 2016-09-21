31af099a7ca11a37d89c50cfa1a4147afa248b94
davidmigloz <me@davidmiguel.com>
2016-09-21 12:50:01 +0200

    Sample project with OpenCV

---
+++ CVtest/app/src/main/java/com/davidmiguel/cvtest/MainActivity.java
@@ -0,0 +1,22 @@
+package com.davidmiguel.cvtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (!OpenCVLoader.initDebug()) {
+            Log.e(this.getClass().getSimpleName(), "  OpenCVLoader.initDebug(), not working.");
+        } else {
+            Log.d(this.getClass().getSimpleName(), "  OpenCVLoader.initDebug(), working.");
+        }
+    }
+}
